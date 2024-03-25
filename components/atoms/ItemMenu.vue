154b918032c33ab41a669c33075c3760d42761b6
Denis78-sys <denismarques736gmail.com>
2024-03-25 09:21:08 -0300

    3º commit, menu hamburguer concluído

--- components/atoms/ItemMenu.vue
+++ components/atoms/ItemMenu.vue
@@ -1,21 +1,19 @@
 <!-- eslint-disable vue/require-prop-types -->
 <template>
-    <div class="item-menu">
-        <a class="text-base" :href="linkM">{{ itemM }}</a>
-    </div>
+  <div class="item-menu hover:bg-azul-300 hover:text-branco rounded">
+    <a class="text-base" :href="linkM">{{ itemM }}</a>
+  </div>
 </template>
 
 <script>
 export default {
-    props:{
-         // eslint-disable-next-line vue/require-prop-types
-        itemM:{Type: String, required: true},
-        // eslint-disable-next-line vue/require-prop-types
-        linkM:{Type: String, required: true}
-    }
-}
+  props: {
+    // eslint-disable-next-line vue/require-prop-types
+    itemM: { Type: String, required: true },
+    // eslint-disable-next-line vue/require-prop-types
+    linkM: { Type: String, required: true },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
--- components/molecules/Menu.vue
+++ components/molecules/Menu.vue
@@ -1,29 +1,50 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="menu">
-         <ul class="md:flex md:flex-row sm:flex-col md:gap-12 sm:gap-x-5">
-            <li v-for="item in itens" :key="item.id">
-               <!-- eslint-disable-next-line vue/attribute-hyphenation -->
-                <ItemMenu :itemM="item.nome" :linkM="item.linkPagina"/>
-            </li>
-         </ul>
-    </div>
+  <div class="menu">
+    <nav class="flex gap-8">
+      <ul
+        class="nav md:flex md:flex-row sm:flex-col md:gap-12 sm:gap-x-5"
+        :class="{ hidden: !menuAtivo }"
+      >
+        <li v-for="item in itens" :key="item.id">
+          <!-- eslint-disable-next-line vue/attribute-hyphenation -->
+          <ItemMenu :itemM="item.nome" :linkM="item.linkPagina" />
+        </li>
+      </ul>
+      <span class="md:hidden sm:block cursor-pointer" @click="ativarMenu()">
+        <img ref="icone" src="@/assets/images/hamburguer.svg" alt="Botão" />
+      </span>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            itens: [{ id: 1, nome: "Home", linkPagina: "" },
-            { id: 2, nome: "Sobre nós", linkPagina: "" },
-            { id: 3, nome: "Serviço", linkPagina: "" },
-            { id: 4, nome: "Contato", linkPagina: "" }
-        ]
-        };
-    }
-}
+  data() {
+    return {
+      itens: [
+        { id: 1, nome: "Home", linkPagina: "" },
+        { id: 2, nome: "Sobre nós", linkPagina: "" },
+        { id: 3, nome: "Serviço", linkPagina: "" },
+        { id: 4, nome: "Contato", linkPagina: "" },
+      ],
+      menuAtivo: false, // Inicialmente o menu está inativo
+    };
+  },
+
+  methods: {
+    ativarMenu() {
+      this.menuAtivo = !this.menuAtivo; // Inverte o estado do menu
+      const icon = this.$refs.icone;
+
+      if (this.menuAtivo === true) {
+        icon.src = require("@/assets/images/close.svg");
+      } else {
+        icon.src = require("@/assets/images/hamburguer.svg");
+      }
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
