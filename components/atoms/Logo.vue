b62fc873136438e019ee4c99673353f67edb0550
Denis78-sys <denismarques736gmail.com>
2024-03-25 11:14:26 -0300

    7º commit, ajustando a logo

--- components/atoms/Logo.vue
+++ components/atoms/Logo.vue
@@ -1,8 +1,8 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="logo">
+  <div class="logo lg:w-48 md:w-36 sm:w-32">
     <a href="">
-      <img src="@/assets/images/LOGO.svg" alt="Logo" />
+      <img src="@/assets/images/LOGO1.svg" alt="Logo" />
     </a>
   </div>
 </template>
